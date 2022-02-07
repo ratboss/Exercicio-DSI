127ae06c680677a47083a69962168e89ccdb0409
yllew <96453340+ratboss@users.noreply.github.com>
2022-02-07 17:47:24 -0300

    Add files via upload

---
+++ Exercício/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Exercício
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_anoAtual = new System.Windows.Forms.TextBox();
+            this.txt_anoNascimento = new System.Windows.Forms.TextBox();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_anoAtual
+            // 
+            this.txt_anoAtual.Location = new System.Drawing.Point(107, 46);
+            this.txt_anoAtual.Name = "txt_anoAtual";
+            this.txt_anoAtual.Size = new System.Drawing.Size(100, 20);
+            this.txt_anoAtual.TabIndex = 0;
+            // 
+            // txt_anoNascimento
+            // 
+            this.txt_anoNascimento.Location = new System.Drawing.Point(107, 6);
+            this.txt_anoNascimento.Name = "txt_anoNascimento";
+            this.txt_anoNascimento.Size = new System.Drawing.Size(100, 20);
+            this.txt_anoNascimento.TabIndex = 1;
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(107, 91);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ano Nascimento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ano Atual";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Idade";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(72, 149);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.TabIndex = 6;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 190);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.txt_anoNascimento);
+            this.Controls.Add(this.txt_anoAtual);
+            this.Name = "Form1";
+            this.Text = "Exercício";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_anoAtual;
+        private System.Windows.Forms.TextBox txt_anoNascimento;
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_calcular;
+    }
+}
+

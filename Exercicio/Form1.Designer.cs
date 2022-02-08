e60e1ce0a24f5b6b1fe7b406a7a384cf23caf7e3
yllew <96453340+ratboss@users.noreply.github.com>
2022-02-07 21:06:46 -0300

    Add files via upload

---
+++ Exercicio/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Exercicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.anoNascimento = new System.Windows.Forms.Label();
+            this.txt_ano_nascimento = new System.Windows.Forms.TextBox();
+            this.anoAtual = new System.Windows.Forms.Label();
+            this.txt_ano_atual = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.idade = new System.Windows.Forms.Label();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Endereço";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefone";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cidade";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(71, 113);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Estado";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(71, 147);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // anoNascimento
+            // 
+            this.anoNascimento.AutoSize = true;
+            this.anoNascimento.Location = new System.Drawing.Point(13, 180);
+            this.anoNascimento.Name = "anoNascimento";
+            this.anoNascimento.Size = new System.Drawing.Size(85, 13);
+            this.anoNascimento.TabIndex = 10;
+            this.anoNascimento.Text = "Ano Nascimento";
+            // 
+            // txt_ano_nascimento
+            // 
+            this.txt_ano_nascimento.Location = new System.Drawing.Point(104, 177);
+            this.txt_ano_nascimento.Name = "txt_ano_nascimento";
+            this.txt_ano_nascimento.Size = new System.Drawing.Size(100, 20);
+            this.txt_ano_nascimento.TabIndex = 11;
+            // 
+            // anoAtual
+            // 
+            this.anoAtual.AutoSize = true;
+            this.anoAtual.Location = new System.Drawing.Point(16, 208);
+            this.anoAtual.Name = "anoAtual";
+            this.anoAtual.Size = new System.Drawing.Size(53, 13);
+            this.anoAtual.TabIndex = 12;
+            this.anoAtual.Text = "Ano Atual";
+            // 
+            // txt_ano_atual
+            // 
+            this.txt_ano_atual.Location = new System.Drawing.Point(74, 206);
+            this.txt_ano_atual.Name = "txt_ano_atual";
+            this.txt_ano_atual.Size = new System.Drawing.Size(100, 20);
+            this.txt_ano_atual.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // idade
+            // 
+            this.idade.AutoSize = true;
+            this.idade.Location = new System.Drawing.Point(19, 237);
+            this.idade.Name = "idade";
+            this.idade.Size = new System.Drawing.Size(34, 13);
+            this.idade.TabIndex = 15;
+            this.idade.Text = "Idade";
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(74, 237);
+            this.txt_idade.Multiline = true;
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.ReadOnly = true;
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 311);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.idade);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_ano_atual);
+            this.Controls.Add(this.anoAtual);
+            this.Controls.Add(this.txt_ano_nascimento);
+            this.Controls.Add(this.anoNascimento);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Exercício";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label anoNascimento;
+        private System.Windows.Forms.TextBox txt_ano_nascimento;
+        private System.Windows.Forms.Label anoAtual;
+        private System.Windows.Forms.TextBox txt_ano_atual;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label idade;
+        private System.Windows.Forms.TextBox txt_idade;
+    }
+}
+
---
+++ Exercicio/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int anoNascimento = Convert.ToInt32(txt_ano_nascimento.Text);
+            int anoAtual = Convert.ToInt32(txt_ano_atual.Text);
+
+            int idade;
+
+            idade = anoAtual - anoNascimento;
+
+            txt_idade.Text = idade.ToString();
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
